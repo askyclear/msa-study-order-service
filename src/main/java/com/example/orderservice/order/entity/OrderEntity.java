60e34e409898c4205ebf6cb5b3a4036a5a367b5c
askyclear <askyclear2@gmail.com>
2022-08-10 22:57:09 +0900

    [인프런] SpringCloud로 개발하는 마이크로서비스 애플리케이션(MSA)
    -- schema 변경

--- src/main/java/com/example/orderservice/order/entity/OrderEntity.java
+++ src/main/java/com/example/orderservice/order/entity/OrderEntity.java
@@ -13,7 +13,7 @@ public class OrderEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false, length = 120, unique = true)
+    @Column(nullable = false, length = 120)
     private String productId;
     @Column(nullable = false)
     private Integer qty;
